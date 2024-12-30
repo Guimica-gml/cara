6779ec66b2f645c38668151ded923c4eac285e1b
ArtemisYo <art.stellata@proton.me>
2024-12-31 00:48:26 +0100

    added file input

--- src/main.c
+++ src/main.c
@@ -1,17 +1,38 @@
 #include <stdio.h>
 #include <assert.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #include "./lexer.h"
 #include "./arena.h"
 #include "./parser.h"
 
-int main() {
-    char str[] =
-        "func main() {\n"
-        "    print(\"Hello World!\");\n"
-        "    return;\n"
-        "}";
-    struct Tokens tokens = lex(str);
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        printf("Please provide a filename!\n");
+        return 1;
+    }
+    char* filename = argv[1];
+    struct stat filestat = {0};
+    if (stat(filename, &filestat) == -1) {
+        printf("Could not access file '%s'!\n", filename);
+        return 1;
+    }
+    int filedesc = open(filename, O_RDONLY);
+    char* file = mmap(
+        NULL, filestat.st_size,
+        PROT_READ, MAP_PRIVATE,
+        filedesc, 0
+    );
+
+    /* char str[] = */
+    /*     "func main() {\n" */
+    /*     "    print(\"Hello World!\");\n" */
+    /*     "    return;\n" */
+    /*     "}"; */
+    struct Tokens tokens = lex(file);
     
     printf("[\n");
     struct Tokenstream stream = Tokens_stream(&tokens);
@@ -31,5 +52,7 @@ int main() {
 
     arena_deinit(&ast_arena);
     Tokens_deinit(&tokens);
+    munmap(file, filestat.st_size);
+    close(filedesc);
     return 0;
 }
