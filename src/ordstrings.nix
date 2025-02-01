0a5319ac096de2f6344f74bb49d4d8385d95bd1e
ArtemisYo <art.stellata@proton.me>
2025-02-01 17:31:50 +0100

    clangd formatted my nix files :reeee:

--- src/ordstrings.nix
+++ src/ordstrings.nix
@@ -1,10 +1,10 @@
-{pkgs, serene} : import./ btree / stencil.nix {
-    inherit pkgs serene;
-    include - path =./ ordstrings.h;
-    basetype = "struct Ordstring";
-    branching = 32;
-    cmp = "Ordstring_cmp";
-    print = "Ordstring_print";
-    treename = "Btrings";
-    filename = "btrings";
+{ pkgs, serene }: import ./btree/stencil.nix {
+  inherit pkgs serene;
+  include-path = ./ordstrings.h;
+  basetype = "struct Ordstring";
+  branching = 32;
+  cmp = "Ordstring_cmp";
+  print = "Ordstring_print";
+  treename = "Btrings";
+  filename = "btrings";
 }
